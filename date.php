81b76f2f656453a1e80ba1fb63044f241c0e7f09
chamberskaz0301 <76543076+chamberskaz0301@users.noreply.github.com>
2022-01-05 14:35:35 +0900

    session追加

--- date.php
+++ date.php
@@ -13,37 +13,140 @@
 
     <h2>以下の内容でよろしければ再度、送信ボタンを押して下しさい。</h2>
 
+    <?php
+
+      session_start();
+
+      $html =      array();
+      $_SESSION =  array();
+
+
+      if(isset($_POST['confirm'])){
+         //POSTデータをSESSIONに格納
+          if(isset($_POST['name']) && $_POST['name'] != '')
+          $_SESSION['name'] = $_POST['name'];
+          else
+          $_SESSION['name'] = '';
+
+          if(isset($_POST['name_jp']) && $_POST['name_jp'] != '')
+          $_SESSION['name_jp'] = $_POST['name_jp'];
+          else
+          $_SESSION['name_jp'] = '';
+      
+          if(isset($_POST['age']) && $_POST['age'] != '')
+          $_SESSION['age'] = $_POST['age'];
+          else
+          $_SESSION['age'] = '';
+            
+          if(isset($_POST['gender']) && $_POST['gender'] != '')
+          $_SESSION['gender'] = $_POST['gender'];
+          else
+          $_SESSION['gender'] = '';
+            
+          if(isset($_POST['phone']) && $_POST['phone'] != '')
+          $_SESSION['phone'] = $_POST['phone'];
+          else
+          $_SESSION['phone'] = '';
+            
+          if(isset($_POST['address']) && $_POST['address'] != '')
+          $_SESSION['address'] = $_POST['address'];
+          else
+          $_SESSION['address'] = '';
+      
+          if(isset($_POST['condition']['fever']) && $_POST['condition']['fever'] != '')
+          $_SESSION['condition']['fever'] = $_POST['condition']['fever'];
+          else
+          $_SESSION['condition']['fever']= '';
+      
+          if(isset($_POST['condition']['throat'] ) && $_POST['condition']['throat']  != '')
+          $_SESSION['condition']['throat']  = $_POST['condition']['throat'] ;
+          else
+          $_SESSION['condition']['throat']  = '';
+      
+      
+          if(isset($_POST['condition']['cough'] ) && $_POST['condition']['cough']  != '')
+          $_SESSION['condition']['cough']  = $_POST['condition']['cough'] ;
+          else
+          $_SESSION['condition']['cough']  = '';
+      
+      
+          if(isset($_POST['condition']['spit'] ) && $_POST['condition']['spit']  != '')
+          $_SESSION['condition']['spit']  = $_POST['condition']['spit'] ;
+          else
+          $_SESSION['condition']['spit']  = '';
+      
+          if(isset($_POST['treat']['none']) && $_POST['treat']['none'] != '')
+          $_SESSION['treat']['none'] = $_POST['treat']['none'];
+          else
+          $_SESSION['treat']['none'] = '';
+      
+      
+          if(isset($_POST['treat']['hbp']) && $_POST['treat']['hbp'] != '')
+          $_SESSION['treat']['hbp'] = $_POST['treat']['hbp'];
+          else
+          $_SESSION['treat']['hbp'] = '';
+      
+      
+          if(isset($_POST['treat']['diabetic']) && $_POST['treat']['diabetic'] != '')
+          $_SESSION['treat']['diabetic'] = $_POST['treat']['diabetic'];
+          else
+          $_SESSION['treat']['diabetic'] = '';
+      
+      
+          if(isset($_POST['treat']['asthma']) && $_POST['treat']['asthma'] != '')
+          $_SESSION['treat']['asthma'] = $_POST['treat']['asthma'];
+          else
+          $_SESSION['treat']['asthma'] = '';
+      
+          if(isset($_POST['allergy']) && $_POST['allergy'] != '')
+          $_SESSION['allergy'] = $_POST['allergy'];
+          else
+          $_SESSION['allergy'] = '';
+
+         }else{
+            ?>
+            確認ボタンを押して下さい。<br/>
+            <a href="phpsample-form-session.php">入力へ</a>
+            </div>
+            <hr/>
+            </body>
+            </html>
+            <?php
+            exit;
+        }
+
+    ?>
     
 
     <form action="pdf.php" method="get">
         <p>
-           氏名:<?php echo $_GET["name"]; ?> 
+           氏名:<?php echo $_SESSION['name']; ?> 
         </p>
         
         <p>
-           しめい:<?php echo $_GET["name_jp"]; ?> 
+           しめい:<?php echo $_SESSION['name_jp']; ?> 
         </p>
         
         <p>
-           生年月日:<?php echo $_GET["age"]; ?> 
+           生年月日:<?php echo $_SESSION['age']; ?> 
         </p>        
         <p>
-           性別:<?php echo $_GET["sex"]; ?> 
+           性別:<?php echo $_SESSION['gender']; ?> 
         </p>
                 
         <p>
-           電話番号:<?php echo $_GET["phone"]; ?> 
+           電話番号:<?php echo $_SESSION['phone']; ?> 
         </p>
         
                 
         <p>
-           住所:<?php echo $_GET["address"]; ?> 
+           住所:<?php echo $_SESSION['address']; ?> 
         </p>
 
 
 
         <p>どのような状況ですか？<br>
-        <?php $chk_condition = filter_input(INPUT_GET, 'condition', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+        <?php $chk_condition = filter_input(INPUT_POST, 'condition', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
             if (!empty($chk_condition)) {
             foreach($chk_condition as $key => $val){
                echo $val."<br />";
@@ -53,7 +156,7 @@
 
         <!-- はい、いいえの分岐処理 -->
         <p>現在治療中の病気はありますか？<br>
-        <?php $chk_treat = filter_input(INPUT_GET, 'treat', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+        <?php $chk_treat = filter_input(INPUT_POST, 'treat', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
             if (!empty($chk_treat)) {
             foreach($chk_treat as $key => $val){
                echo $val."<br />";
@@ -62,7 +165,7 @@
 
 
         <p>アレルギー体質ですか？<br>        
-        <?php echo $_GET["allergy"]; ?> 
+        <?php echo $_SESSION['allergy']; ?> 
 
         </p>
         
@@ -72,6 +175,11 @@
         
 
     </form>
+
+    <form action="form.php" method="POST">
+      <input type="submit" name="back" value="-　戻る　-">
+   </form>
+
     
 </body>
 </html>
--- form.php
+++ form.php
@@ -1,20 +1,4 @@
-<?php
-    session_start();
-    $error = "";
-    $name = $_POST["name"];
-    $name_jp = $_POST["name_jp"];
-    $age = $_POST["age"];
-    $sex = $_POST["sex"];
-    $phone = $_POST["phone"];
-    $address = $_POST["address"];
-    $condition = $_POST["condition"];
-    $treat = $_POST["treat"];
-    $allergy = $_POST["allergy"];
 
-    
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,6 +10,51 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    <?php
+    session_start();
+
+    $html = array();
+
+    if(isset($_POST['back'])){
+        //htmlentities()でHTMLコードをエスケープします。
+        $html['name'] =               htmlentities($_SESSION['name'], ENT_QUOTES, 'UTF-8');
+        $html["name_jp"] =    htmlentities($_SESSION["name_jp"], ENT_QUOTES, 'UTF-8');
+        $html['age'] = htmlentities($_SESSION["age"], ENT_QUOTES, 'UTF-8');
+        $html["gender"] =       htmlentities($_SESSION["gender"], ENT_QUOTES, 'UTF-8');
+        $html["phone"] =           htmlentities($_SESSION["phone"], ENT_QUOTES, 'UTF-8');
+        $html["address"] =           htmlentities($_SESSION["address"], ENT_QUOTES, 'UTF-8');
+        $html['condition']['fever'] =           htmlentities($_SESSION["condition"]['fever'], ENT_QUOTES, 'UTF-8');
+        $html['condition']['throat'] =           htmlentities($_SESSION["condition"]['throat'], ENT_QUOTES, 'UTF-8');
+        $html['condition']['cough'] =           htmlentities($_SESSION["condition"]['cough'], ENT_QUOTES, 'UTF-8');
+        $html['condition']['spit'] =           htmlentities($_SESSION["condition"]['spit'], ENT_QUOTES, 'UTF-8');
+        $html['treat']['none'] =           htmlentities($_SESSION['treat']['none'], ENT_QUOTES, 'UTF-8');
+        $html['treat']['hbp'] =           htmlentities($_SESSION['treat']['hbp'], ENT_QUOTES, 'UTF-8');
+        $html['treat']['diabetic'] =           htmlentities($_SESSION['treat']['diabetic'], ENT_QUOTES, 'UTF-8');
+        $html['treat']['asthma'] =           htmlentities($_SESSION['treat']['asthma'], ENT_QUOTES, 'UTF-8');
+        $html["allergy"] =           htmlentities($_SESSION["allergy"], ENT_QUOTES, 'UTF-8');
+    //初期値
+    }else{
+        //初期化
+        $_SESSION =  array();
+        $html['name'] =  '';
+        $html['name_jp'] = '';
+        $html['age'] = '';
+        $html['gender'] = '';
+        $html['phone'] =  '';
+        $html['address'] =  '';
+        $html['condition']['fever'] = '';
+        $html['condition']['throat'] =  '';
+        $html['condition']['cough'] = '';
+        $html['condition']['spit'] =  '';
+        $html['treat']['none'] = '';
+        $html['treat']['hbp'] =  '';
+        $html['treat']['diabetic'] =  '';
+        $html['treat']['asthma'] =  '';
+        $html['allergy'] =  '';
+    }
+
+
+    ?>
 
     <h1>内科問診票</h1>
 
@@ -35,49 +64,48 @@
 
     <form action="date.php" method="post">
         <p>
-           氏名:<input type="text" name="name"> 
+           氏名:<input type="text" name="name" , value="<?php echo  $html['name']; ?>"> 
         </p>
         
         <p>
-           しめい:<input type="text" name="name_jp"> 
+           しめい:<input type="text" name="name_jp", value="<?php echo  $html['name_jp']; ?>"> 
         </p>
         
         <p>
-           生年月日:<input type="text" name="age" > 
+           年齢:<input type="text" name="age" , value="<?php echo  $html['age']; ?>" > 
         </p>
 
         <p>男性ですか女性ですか？<br>
-            <input type="radio" name="sex" value="男"> 男
-            <input type="radio" name="sex" value="女"> 女
+            <input type="radio" name="gender" value="男"> 男
+            <input type="radio" name="gender" value="女"> 女
         </p>
 
 
                 
         <p>
-           電話番号:<input type="text" name="phone" >
+           電話番号:<input type="text" name="phone" , value="<?php echo  $html['phone']; ?>" >
         </p>
         
                 
         <p>
-           住所:<input type="text" name="address"> 
+           住所:<input type="text" name="address" , value="<?php echo  $html['address']; ?>">  
         </p>
 
 
         <p>どのような状況ですか？<br>
-            <input type="checkbox" name="condition[]" value="発熱" >発熱
-            <input type="checkbox" name="condition[]" value="のどの痛み">のどの痛み
-            <input type="checkbox" name="condition[]" value="せき">せき
-            <input type="checkbox" name="condition[]" value="たん">たん
-            <input type="checkbox" name="condition[]" value="せき">せき
+            <input type="checkbox" name="condition[fever]" value="fever"<?php if($html['condition']['fever'] == 'fever') echo ' checked';?> >発熱
+            <input type="checkbox" name="condition[throat]" value="throat"<?php if($html['condition']['throat'] == 'throat') echo ' checked';?>>のどの痛み
+            <input type="checkbox" name="condition[cough]" value="cough"<?php if($html['condition']['cough'] == 'cough') echo ' checked';?>>せき
+            <input type="checkbox" name="condition[spit]" value="spit"<?php if($html['condition']['spit'] == 'spit') echo ' checked';?>>たん
         </p>
 
         <!-- はい、いいえの分岐処理 -->
         <p>現在治療中の病気はありますか？<br>
-            <input type="checkbox" name="treat[]" value="なし" >なし
-            <input type="checkbox" name="treat[]" value="あり">あり
-            <input type="checkbox" name="treat[]" value="高血圧">高血圧
-            <input type="checkbox" name="treat[]" value="糖尿病">糖尿病
-            <input type="checkbox" name="treat[]" value="ぜんそく">ぜんそく
+            <input type="checkbox" name="treat[none]" value="none"<?php if($html['treat']['none'] == 'none') echo ' checked';?> >なし
+            ・ありの方は下記から選んでください。<br/>
+            <input type="checkbox" name="treat[hbp]" value="hbp"<?php if($html['treat']['hbp'] == 'hbp') echo ' checked';?>>高血圧
+            <input type="checkbox" name="treat[diabetic]" value="diabetic"<?php if($html['treat']['diabetic'] == 'diabetic') echo ' checked';?>>糖尿病
+            <input type="checkbox" name="treat[asthma]" value="asthma"<?php if($html['treat']['asthma'] == 'asthma') echo ' checked';?>>ぜんそく
         </p>
 
 
@@ -87,7 +115,7 @@
         </p>
         
 
-        <input type="submit" value="送信する">
+        <input type="submit" name="confirm" value="送信する">
 
         
 
